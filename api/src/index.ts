56988b5bb6d2b0ef33cf1e681907f7e23fb2c096
ahmad <ahmad2smile@protonmail.com>
2020-02-06 13:19:53 +0500

    Init commit

---
+++ api/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
